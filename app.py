201b316590240a5d9e52841d376a27a194b7305a
Cory Robbins <coryreedrobbins@live.com>
2023-03-03 12:58:40 +0100

    Revert "Finally labeling but the next button is faulty"
    This reverts commit 6838575740b2ddce72f3f36eb6c2b7149208b333.

--- app.py
+++ app.py
@@ -1,83 +1,85 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+#import pdb; pdb.set_trace()
 
 app = Flask(__name__)
-
-def get_labels(email):
-    labels= ['Label 1','Label 2','Label 3', 'Label 4', 
-             'Label 5', 'Label 6','Label 7', 'Label 8',
-             'Label 9', 'Label 10','Label 11', 'Label 12',
-             'Label 13', 'Label 14','Non-Technical'
-            ]
-    return labels
+labels = ['Label 1','Label 2','Label 3', 'Label 4', 
+          'Label 5', 'Label 6','Label 7', 'Label 8',
+          'Label 9', 'Label 10','Label 11', 'Label 12',
+          'Label 13', 'Label 14','Non-Technical'
+          ]
 
 def load_dataframe():
     file_name = 'emails.csv'
     df = pd.read_csv(file_name)
     return df
 
-def save_dataframe(df):
-    file_name = 'emails.csv'
-    df.to_csv(file_name, index=False)
-
 def get_email_by_index(df, index):
     email = df.loc[df.index == index, 'body'].iloc[0]
     return email
 
 def get_previous_email_index(df, index):
-    previous_index = df.index[df.index.get_loc(index) - 1] if df.index.get_loc(index) > 0 else index
+    previous_index = df.index[df.index < index][-1]if df.index[df.index < index].size > 0 else index
     return previous_index
 
 @app.route('/')
-def index():
+@app.route('/index/<int:index>')
+def index(index=None):
     df = load_dataframe()
-    index = int(request.args.get('index') or 0)
-    email = df.loc[index, 'body']
-    assigned_label = df.loc[index, 'label'] or 'Not Assigned'
-    labels = get_labels(email)
+    if index is None:
+        index = get_next_email_index(df, -1)
+    if index is None:
+        return "No more emails to label."
+    email = get_email_by_index(df, index)
+    assigned_label = df.loc[index, 'label']
+    if pd.isna(assigned_label):
+        assigned_label = 'Not Assigned'
+    return render_template("index.html", email=email, labels=labels, assigned_index=index, assigned_label=assigned_label)
 
-    labeled_indices = df[df['label'].notnull()].index.tolist()
-    assigned_index = labeled_indices.index(index) if index in labeled_indices else -1
+def get_next_email_index(df, index):
+    """
+    Get the next email index greater than the given index.
 
-    return render_template('index.html', email=email, assigned_label=assigned_label, labels=labels, assigned_index=assigned_index, index=index)
+    Args:
+        df (pandas.DataFrame): Dataframe containing the email data.
+        index (int or callable): Index of the email to get the next index for.
 
-def get_next_email_index(df, index):    
+    Returns:
+        int: Next email index.
+    """
     if callable(index):
         index = index()
     next_index = df.index[df.index > index]
     if next_index.size == 0:
-        return None 
+        return "No more emails to label." 
     else:    
         return next_index[0]
     
 @app.route('/label', methods=['POST'])
 def label():
     df = load_dataframe()
-    index = int(request.form.get('index') or 0)
-  
+    index = request.form.get('index', 0)
+    index = int(index)
+    print(f"Index: {index}")
     if 'label' in request.form:
         label = request.form['label'].strip()
-        labels = get_labels(df.loc[index, 'body'])
         if label in labels:
+            print(f"Index: {index}")
             df.loc[index, 'label'] = label
-            save_dataframe(df)
-        else:
-            df.at[index, 'label'] = 'Not Assigned'
-            save_dataframe(df)
+            df.to_csv('emails.csv', index=False)
+            next_index = get_next_email_index(df, index)
+            print(f"Next Index: {next_index}")
+            return redirect(url_for('index', index=next_index))
+    elif 'next_email' in request.form:
+        print(f"Before calling get_next_email_index: index = {index}")
         next_index = get_next_email_index(df, index)
+        print(f"After calling get_next_email_index: next_index = {next_index}")
         return redirect(url_for('index', index=next_index))
     elif 'previous_email' in request.form:
-        try:
-            previous_index = get_previous_email_index(df, index)
-        except KeyError:
-            previous_index = get_previous_email_index(df, index-1)
+        previous_index = get_previous_email_index(df, index)
+        print(f"Previous Index: {previous_index}")
         return redirect(url_for('index', index=previous_index))
-    elif 'next_email' in request.form:
-        next_index = get_next_email_index(df, index)
-        return redirect(url_for('index', index=next_index))
-    else:
-        # Return a default response if no action was taken
-        return redirect(url_for('index', index=index))
+    return redirect(url_for('index', index=index))
 
 if __name__ == '__main__':
     app.run(debug=True)
